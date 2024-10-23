d0e064433a385b758b4e711a8e59cf0fc050daa2
Isaac Mayernik <mayernii@email.sc.edu>
2024-10-23 10:33:15 -0400

    attempting conflict

--- app/App.js
+++ app/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.title}>Hello, Cruel World!</Text>
       <Text>Ryan Was here! And I'm solving a merge conflict! and johnny</Text>
-      <Text>Isaac was here!</Text>
+      <Text>Isaac was here! resolving conflict</Text>
       <Text>Jacob was here! banana apples</Text>
       <Text style={styles.johnny}>Johnny was here! as was Jacob hahaha lmaorofl i love pizza</Text>
       <StatusBar style="auto" />
