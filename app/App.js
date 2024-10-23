2f895068e770986286c91558e28edd4d7f63f42b
Ryan Malone <ryan.malone192@gmail.com>
2024-10-23 10:45:16 -0400

    test

--- app/App.js
+++ app/App.js
@@ -6,12 +6,14 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.title}>Hello, Cruel World!</Text>
       <Text>Ryan Was here! And I'm solving a merge conflict! and johnny</Text>
-      <Text>Isaac was here! Hi Isaac, Ryan was here! resolving conflict. FUSION!!!! merging branch</Text>
+      <Text>Isaac was here! Hi Isaac, Ryan was here! resolving conflict</Text>
       <Text>Jacob was here! banana apples</Text>
       <Text style={styles.johnny}>
         Johnny was here! as was Jacob hahaha lmaorofl i love pizza
       </Text>
-      <Text style={styles.johnny}>This is Johnny's code merging to main from my branch</Text>
+      <Text style={styles.johnny}>
+        This is Johnny's code merging to main from my branch
+      </Text>
       <Text>This was creates on Ryan's branch to merge into main</Text>
       <StatusBar style="auto" />
     </View>
