8c9ce2523b48c90ef23216ec0c176148ad513001
Ryan Malone <ryan.malone192@gmail.com>
2024-10-23 17:01:03 -0400

    Added start of hook for api

---
+++ hook/fetchInfo.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const fetchInfo = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // We will have to change this to take in a subject as a parameter
+  // (or other parameters that the api supports)
+  const fetch = async () => {
+    setIsLoading(true);
+    try {
+      console.log("Fetching data...");
+      const response = await axios({
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "https://classes.sc.edu/api/?page=fose&route=search&subject=CSCE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+          other: {
+            srcdb: "999999",
+          },
+          criteria: [
+            {
+              field: "subject",
+              value: "CSCE",
+            },
+          ],
+        }),
+      });
+      const parsedData =
+        typeof response.data === "string"
+          ? JSON.parse(response.data)
+          : response.data;
+      setData(parsedData);
+      console.log("Fetch successful:");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  return { data, isLoading, error };
+};
+
+export default fetchInfo;
