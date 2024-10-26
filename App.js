8441c3100c2f8e69ade0780d7b650a6088947a40
Ryan Malone <ryan.malone192@gmail.com>
2024-10-26 09:34:31 -0400

    Changed eas and app.json to reflect new organization on expo.dev

--- App.js
+++ App.js
@@ -6,13 +6,22 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.title}>Hello, Cruel World!</Text>
       <Text>Ryan Was here! And I'm solving a merge conflict! and johnny</Text>
-      <Text>Jacob was here! banana apples Raffi was lit baby beluga I have a branch I have a branch</Text>
-      <Text>Isaac was here! Hi Isaac, Ryan was here! resolving conflict. FUSION!!! HAA!!!!</Text>
+      <Text>
+        Jacob was here! banana apples Raffi was lit baby beluga I have a branch
+        I have a branch
+      </Text>
+      <Text>
+        Isaac was here! Hi Isaac, Ryan was here! resolving conflict. FUSION!!!
+        HAA!!!!
+      </Text>
       <Text style={styles.johnny}>
         Johnny was here! as was Jacob hahaha lmaorofl i love pizza
       </Text>
-      <Text style={styles.johnny}>This is Johnny's code merging to main from my branch (for the second time)</Text>
-      <Text>This was creates on Ryan's branch to merge into main</Text>
+      <Text style={styles.johnny}>
+        This is Johnny's code merging to main from my branch (for the second
+        time)
+      </Text>
+      <Text>This was created on Ryan's branch to merge into main</Text>
       <StatusBar style="auto" />
     </View>
   );
