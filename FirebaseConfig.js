bebc7cb24ebf08f70822ecf25427909bb718b6eb
CyberneticMedic <jccbagley@protonmail.com>
2024-11-08 06:35:05 -0900

    research Repo

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,10 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
+
 import {
   getAuth,
   initializeAuth,
   getReactNativePersistence,
 } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
