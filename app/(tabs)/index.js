16bfefb34410503223169fbf097dce43084f9e13
Isaac Mayernik <mayernii@email.sc.edu>
2024-11-10 20:48:16 -0500

    Set up map view and a marker to test

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,10 +1,51 @@
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { useNavigation } from 'expo-router';
+import { markers } from '../../assets/markers';
+import Geolocation from '@react-native-community/geolocation';
+
+//Map page
+
+const INITIAL_REGION = {
+  latitude: 34.00039991787572,
+  longitude: -81.03594096158815,
+  latitudeDelta: 0.05,
+  longitudeDelta: 0.05
+}
+
+const onMarkerSelected = (marker) => {
+  Alert.alert(marker.name);
+};
+
+// need to get permissions of user
+// const getUserCurrentLocation = () => {
+//   Geolocation.getCurrentPosition(position => {
+//     console.log(position);
+//   });
+// };
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+  // getUserCurrentLocation();
+
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
+    // provider={PROVIDER_GOOGLE}
+    // this requires using Google's Map APIs, which has a free trial
+    // trial is a very key word
+    <SafeAreaView style={styles.container}>
+      <MapView style ={styles.map} 
+        initialRegion={INITIAL_REGION}
+        showsUserLocation
+        showsMyLocationButton 
+        mapType="mutedStandard"
+      >
+        {markers.map((marker, index) => (
+          <Marker key={index} coordinate={marker}
+          onPress={() => onMarkerSelected(marker)} />
+        ))}
+        </MapView>
+    </SafeAreaView>
   );
 }
 
@@ -13,5 +54,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
---
+++ assets/markers.ts
@@ -0,0 +1,9 @@
+export const markers = [
+    {
+        latitude: 33.99200436583021, 
+        longitude: -81.03598623688623,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+        name: 'Colonial Life Student Parking'
+    }
+]
