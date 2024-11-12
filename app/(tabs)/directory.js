72f56a14def47913e7976d19c0c40bda40c17b2d
Ryan Malone <ryan.malone192@gmail.com>
2024-11-11 20:51:36 -0500

    Formated names, hardcoded data, and added search bar

--- app/(tabs)/directory.js
+++ app/(tabs)/directory.js
@@ -7,6 +7,7 @@ import {
   SafeAreaView,
   ScrollView,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { FIRESTORE_DB } from "../../FirebaseConfig";
@@ -15,12 +16,19 @@ import { useRouter } from "expo-router";
 import getItemLayout from "react-native-section-list-get-item-layout";
 
 export default function Directory() {
+  // Used for navigation
   const router = useRouter();
 
+  // Used for fetching data from Firestore DB
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // Used for search feature
+  const [search, setSearch] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+  // Fetch data from Firestore DB
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -28,6 +36,8 @@ export default function Directory() {
           collection(FIRESTORE_DB, "staff-directory")
         );
         const db_data = query.docs.map((doc) => doc.data());*/
+
+        // Placeholder data to prevent quota overage
         const db_data = [
           {
             College: "School of Public Health",
@@ -53,6 +63,74 @@ export default function Directory() {
             Tags: "Health Promotion Education and Behavior,",
             Title: "Associate Professor",
           },
+          {
+            "Faculty/Staff": "Faculty",
+            Name: "Anderson, Brandi, LICSW, SSW, CFSW, RYT",
+            Title:
+              "Director of the I. DeQuincey Newman Institute and Director of Innovative ACCESS (Access, Culture, Community Engagement, and Student Success)",
+            "Secondary Title": "",
+            Phone: "777-2064",
+            Email: "bbrisker@email.sc.edu",
+            Keywords: "",
+            Department: "",
+            College: "College of Social Work",
+          },
+          {
+            "Faculty/Staff": "Faculty",
+            Name: "Bell, Nathaniel",
+            Title: "Director of Research and Evaluation",
+            "Secondary Title": "",
+            Phone: "803-576-6781",
+            Email: "nateb@mailbox.sc.edu",
+            Keywords: "",
+            Department: "IFS/IHPR Institute for Families in Society",
+            College: "College of Social Work",
+          },
+          {
+            "Faculty/Staff": "Faculty",
+            Name: "Bosio-McArdle, Jennifer , MSW",
+            Title: "Interim Undergraduate Program Director/Lecturer",
+            "Secondary Title": "",
+            Phone: "803-777-0383",
+            Email: "bosio@mailbox.sc.edu",
+            Keywords: "",
+            Department: "",
+            College: "College of Social Work",
+          },
+          {
+            "Faculty/Staff": "Faculty",
+            Name: "Browne, Teri, Ph.D., MSW",
+            Title: "Dean",
+            "Secondary Title": "Professor",
+            Phone: "803-777-6258",
+            Email: "brownetm@mailbox.sc.edu",
+            Keywords:
+              "professor, academic, teri, browne, associate, dean, faculty, research",
+            Department: "",
+            College: "College of Social Work",
+          },
+          {
+            "Faculty/Staff": "Staff",
+            Name: "Carter-Moore, Felissa",
+            Title: "Assistant Dean for Finance and Administration",
+            "Secondary Title": "",
+            Phone: "803-777-3349",
+            Email: "fcarter@mailbox.sc.edu",
+            Keywords: "administrative, dean, finance, administration",
+            Department: "",
+            College: "College of Social Work",
+          },
+          {
+            "Faculty/Staff": "Staff",
+            Name: "Cato, Camea, M.Ed.",
+            Title: "Graduate Academic Advisor",
+            "Secondary Title": "",
+            Phone: "803-777-6492",
+            Email: "catocam@mailbox.sc.edu",
+            Keywords: "",
+            Department: "",
+            College: "College of Social Work",
+          },
         ];
         setData(db_data);
       } catch (err) {
@@ -65,6 +143,23 @@ export default function Directory() {
 
     fetchData();
   }, []);
+
+  // Filter data based on value in search bar. If search bar is empty, show all data
+  useEffect(() => {
+    if (search === "") {
+      setFilteredData(data);
+    } else {
+      const filtered = data.filter((item) =>
+        item.Name.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredData(filtered);
+    }
+
+    const filtered = data.filter((item) =>
+      item.Name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredData(filtered);
+  }, [search, data]);
 
   if (isLoading) {
     return (
@@ -82,6 +177,16 @@ export default function Directory() {
     );
   }
 
+  // Format name to be First Last (and cuts off titles, middle initials, etc.)
+  const formatName = (name) => {
+    const parts = name.split(/[\s,]+/);
+    if (parts.length >= 2) {
+      return `${parts[1]} ${parts[0]}`;
+    } else {
+      return name;
+    }
+  };
+
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   const sections = alphabet.map((letter) => ({
     title: letter,
@@ -90,9 +195,15 @@ export default function Directory() {
 
   return (
     <SafeAreaView style={styles.container}>
+      <TextInput
+        placeholder="Search by Name"
+        value={search}
+        onChangeText={(text) => setSearch(text)}
+        style={styles.searchBar}
+      />
       <ScrollView contentContainerStyle={styles.scrollViewContainer}>
         {console.log(data)}
-        {data.map((item) => (
+        {filteredData.map((item) => (
           <TouchableOpacity
             key={item.Name}
             style={styles.staffBox}
@@ -103,7 +214,7 @@ export default function Directory() {
               })
             }
           >
-            <Text style={styles.staffText}>{item.Name}</Text>
+            <Text style={styles.staffText}>{formatName(item.Name)}</Text>
             <FontAwesome name="chevron-right" size={30} color="#fff" />
           </TouchableOpacity>
         ))}
@@ -117,6 +228,15 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: "center",
     // alignItems: "center",
+  },
+  searchBar: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    margin: 10,
+    width: "90%",
   },
   staffBox: {
     padding: 20,
