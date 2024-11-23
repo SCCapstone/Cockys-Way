0e937465011ad1428a2a2e2ff2a4d3048b730e58
jhyman97 <JPHYMAN@email.sc.edu>
2024-11-22 19:46:30 -0500

    added filtering options for class search by semester

--- hook/fetchInfo.js
+++ hook/fetchInfo.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const fetchInfo = (subject) => {
+const fetchInfo = (subject, semester) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
@@ -23,7 +23,7 @@ const fetchInfo = (subject) => {
         },
         data: JSON.stringify({
           other: {
-            srcdb: "999999",
+            srcdb: `${semester}`,
           },
           criteria: [
             {
