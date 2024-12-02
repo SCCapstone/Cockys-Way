a36d3d0fc6b9137aae391af33cf3046746c76442
Ryan Malone <ryan.malone192@gmail.com>
2024-12-02 18:55:20 -0500

    FINALLY fixed our bug with eas and building

---
+++ app.config.js
@@ -0,0 +1,44 @@
+import "dotenv/config"; // Ensure you have dotenv installed to use .env files
+
+export default {
+  expo: {
+    name: "Cockys-Way",
+    slug: "cockys-way",
+    newArchEnabled: true,
+    version: "1.0.0",
+    scheme: "cockysway",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.ryanmalone.CockysWay",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "25d82025-901e-4d91-84eb-966383bd4984",
+      },
+    },
+    owner: "cockys-way",
+    plugins: ["expo-font"],
+  },
+};
