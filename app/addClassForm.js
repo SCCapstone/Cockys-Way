1ac460d90e15f54c09fd56d4858b2696338396ed
Ryan Malone <ryan.malone192@gmail.com>
2025-02-11 22:36:08 -0500

    Added validation. #50 progress

--- app/addClassForm.js
+++ app/addClassForm.js
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
-import { Dropdown } from 'react-native-element-dropdown';
-import { useRouter } from 'expo-router';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { Dropdown } from "react-native-element-dropdown";
+import { useRouter } from "expo-router";
 
 const data = [
-  { label: 'Fall 2024', value: '202408' },
-  { label: 'Spring 2025', value: '202501' }
+  { label: "Fall 2024", value: "202408" },
+  { label: "Spring 2025", value: "202501" },
 ];
 
 const AddClassForm = () => {
@@ -14,65 +20,116 @@ const AddClassForm = () => {
   const [semester, setSemester] = useState(null);
   const [subject, setSubject] = useState("");
   const [number, setNumber] = useState("");
+  const [errors, setErrors] = useState({});
+
+  // Validate inputs for errors
+  const validateInputs = () => {
+    const newErrors = {};
+    if (
+      !semester ||
+      semester.length === 0 ||
+      semester == " " ||
+      semester == ""
+    ) {
+      newErrors.semester = "Semester is required";
+    }
+    if (!subject || subject.length === 0 || subject == " " || subject == "") {
+      newErrors.subject = "Subject is required";
+    }
+    if (
+      (number && isNaN(number)) ||
+      number == " " ||
+      number == "" ||
+      number == null ||
+      number < 101
+    ) {
+      newErrors.number = "Course number must be a number greater than 100";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  // If input is valid, it'll route to the results
+  const handleSubmit = () => {
+    if (validateInputs()) {
+      router.push({
+        pathname: "/addClassSearchResults",
+        params: { semester, subject, number },
+      });
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={[styles.header, styles.center]}>Enter Class Search Criteria:</Text>
-      <Text style={[styles.subheader, styles.center]}>A * denotes a required field.</Text>
+      <Text style={[styles.header, styles.center]}>
+        Enter Class Search Criteria:
+      </Text>
+      <Text style={[styles.subheader, styles.center]}>
+        A * denotes a required field.
+      </Text>
       <View style={styles.form}>
         <Text style={styles.label}>Semester*</Text>
         <Dropdown
-          style={styles.input}
-          placeholderStyle={{ fontSize: 20 }} 
+          style={[styles.input, errors.semester && styles.errorInput]}
+          placeholderStyle={{ fontSize: 20 }}
           selectedTextStyle={{ fontSize: 20 }}
           data={data}
           value={semester}
           labelField="label"
           valueField="value"
           placeholder="Semester"
-          onChange={item => {
-            setSemester(item.value)
+          onChange={(item) => {
+            setSemester(item.value);
+            setErrors({ ...errors, semester: null });
           }}
         />
+        {errors.semester && (
+          <Text style={styles.errorText}>{errors.semester}</Text>
+        )}
+
         <Text style={styles.label}>Subject* (CSCE, ENGL, MATH, etc.)</Text>
-        <TextInput 
-          style={styles.input}
+        <TextInput
+          style={[styles.input, errors.subject && styles.errorInput]}
           placeholder="Subject"
-          onChangeText={text => setSubject(text)}
+          value={subject}
+          onChangeText={(text) => {
+            setSubject(text);
+            setErrors({ ...errors, subject: null });
+          }}
         />
+        {errors.subject && (
+          <Text style={styles.errorText}>{errors.subject}</Text>
+        )}
+
         <Text style={styles.label}>Course Number (101, 240, 567, etc.)</Text>
         <TextInput
-          style={styles.input} 
+          style={[styles.input, errors.number && styles.errorInput]}
           placeholder="Course Number"
           keyboardType="numeric"
-          onChangeText={text => setNumber(text)}
-        />
-        <TouchableOpacity
-          style={styles.submit}
-          onPress={() => {
-            console.log('hello its pressed')
-            // router.push("/addClassSearchResults");
-            router.push({
-              pathname: "/addClassSearchResults",
-              params: { semester, subject, number },
-            });
+          value={number}
+          onChangeText={(text) => {
+            setNumber(text);
+            setErrors({ ...errors, number: null });
           }}
-        >
+        />
+        {errors.number && <Text style={styles.errorText}>{errors.number}</Text>}
+
+        <TouchableOpacity style={styles.submit} onPress={handleSubmit}>
           <Text style={styles.submitText}>Search For Classes</Text>
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default AddClassForm
+export default AddClassForm;
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 30,
     paddingHorizontal: 20,
-    gap: 50, 
-    flex: 1
+    gap: 50,
+    flex: 1,
   },
 
   header: {
@@ -85,9 +142,9 @@ const styles = StyleSheet.create({
   },
 
   center: {
-    textAlign: "center"
+    textAlign: "center",
   },
-  
+
   form: {
     paddingHorizontal: 20,
   },
@@ -107,8 +164,16 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
 
+  errorInput: {
+    borderColor: "red",
+  },
+  errorText: {
+    color: "red",
+    marginBottom: 10,
+  },
+
   submit: {
-    backgroundColor: '#73000A',
+    backgroundColor: "#73000A",
     alignItems: "center",
     borderRadius: 10,
     paddingVertical: 10,
@@ -118,6 +183,5 @@ const styles = StyleSheet.create({
   submitText: {
     color: "#FFFFFF",
     fontSize: 20,
-  }
-
-})
+  },
+});
