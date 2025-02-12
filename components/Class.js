1e3b07ba8e5858122e544c2d7ebac4bbea999396
Ryan Malone <ryan.malone192@gmail.com>
2025-02-12 15:40:26 -0500

    added tests for #49

--- components/Class.js
+++ components/Class.js
@@ -1,161 +1,176 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native'
-import React from 'react'
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import { useState } from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import React from "react";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { useState } from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 import { doc, setDoc, deleteDoc } from "firebase/firestore";
-import { FIRESTORE_DB } from '../FirebaseConfig';
-import { getAuth } from 'firebase/auth';
-import { useRouter } from 'expo-router';
+import { FIRESTORE_DB } from "../FirebaseConfig";
+import { getAuth } from "firebase/auth";
+import { useRouter } from "expo-router";
 
-const Class = ({ crn, code, section, name, instructor, meeting, fromSearch = false, onDeletePress }) => {
-    const [notification, setNotification] = useState(false);
-    const router = useRouter();
+const Class = ({
+  crn,
+  code,
+  section,
+  name,
+  instructor,
+  meeting,
+  fromSearch = false,
+  onDeletePress,
+}) => {
+  const [notification, setNotification] = useState(false);
+  const router = useRouter();
 
-    const addToSchedule = async() => {
+  const addToSchedule = async () => {
+    const db = FIRESTORE_DB;
 
-        const db = FIRESTORE_DB;
+    const auth = getAuth();
+    const user = auth.currentUser;
 
-        const auth = getAuth();
-        const user = auth.currentUser;
-
-        if(!user) {
-            console.log("no user found when trying to add course");
-            return;
-        }
-
-        try {
-            const docRef = doc(db, "schedules", user.uid, "courses", crn);
-            await setDoc(docRef, {
-                code: code,
-                instructor: instructor,
-                meeting: meeting,
-                name: name,
-                section: section
-            });
-        } catch (error) {
-            console.log("error when adding class: " + error);
-        }
-
-        router.push("../(tabs)/schedule");
-
+    if (!user) {
+      console.log("no user found when trying to add course");
+      return;
     }
 
-    const deleteFromSchedule = async() => {
-        const db = FIRESTORE_DB;
-
-        const auth = getAuth();
-        const user = auth.currentUser;
-
-        if(!user) {
-            console.log("no user found when trying to delete course");
-            return;
-        }
-        console.log('crn:' + crn);
-        try {
-            const docRef = doc(db, "schedules", user.uid, "courses", crn);
-            await deleteDoc(docRef);
-        } catch (error) {
-            console.log("error: " + error)
-        }
+    try {
+      const docRef = doc(db, "schedules", user.uid, "courses", crn);
+      await setDoc(docRef, {
+        code: code,
+        instructor: instructor,
+        meeting: meeting,
+        name: name,
+        section: section,
+      });
+    } catch (error) {
+      console.log("error when adding class: " + error);
     }
 
-    return (
+    router.push("../(tabs)/schedule");
+  };
+
+  const deleteFromSchedule = async () => {
+    const db = FIRESTORE_DB;
+
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (!user) {
+      console.log("no user found when trying to delete course");
+      return;
+    }
+    console.log("crn:" + crn);
+    try {
+      const docRef = doc(db, "schedules", user.uid, "courses", crn);
+      await deleteDoc(docRef);
+    } catch (error) {
+      console.log("error: " + error);
+    }
+  };
+
+  return (
     <View style={styles.course}>
       <View style={styles.courseText}>
-        <Text style={styles.courseHeader}>{code}-{section}</Text>
+        <Text style={styles.courseHeader}>
+          {code}-{section}
+        </Text>
         <Text style={styles.courseInfo}>{name}</Text>
         <Text style={styles.courseInfo}>{instructor}</Text>
         <Text style={styles.courseInfo}>{meeting}</Text>
       </View>
       <View style={styles.courseIcons}>
-        {fromSearch ? 
-        <Pressable
+        {fromSearch ? (
+          <Pressable
             onPress={() => {
-                addToSchedule();
+              addToSchedule();
             }}
-            style={({pressed}) => [
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed ? "#450006" : "transparent",
+              },
+              styles.button,
+            ]}
+          >
+            <FontAwesome name="plus-circle" size={30} color="#FFFFFF" />
+          </Pressable>
+        ) : (
+          <>
+            <Pressable
+              onPress={() => {
+                setNotification(!notification);
+              }}
+              style={({ pressed }) => [
                 {
-                backgroundColor: pressed ? '#450006' : 'transparent'
+                  backgroundColor: pressed ? "#450006" : "transparent",
                 },
                 styles.button,
-            ]}
-        >
-            <FontAwesome name="plus-circle" size={30} color="#FFFFFF" />
-        </Pressable> : 
-        <>
-            <Pressable
-                onPress={() => {
-                    setNotification(!notification)
-                }}
-                style={({pressed}) => [
-                    {
-                    backgroundColor: pressed ? '#450006' : 'transparent'
-                    },
-                    styles.button,
-                ]}
+              ]}
             >
-                <FontAwesome5 name={ notification ? "bell" : "bell-slash" } size={30} color="#FFFFFF" />
+              <FontAwesome5
+                name={notification ? "bell" : "bell-slash"}
+                size={30}
+                color="#FFFFFF"
+              />
             </Pressable>
             <Pressable
-                onPress={onDeletePress}
-                style={({pressed}) => [
-                    {
-                    backgroundColor: pressed ? '#450006' : 'transparent'
-                    },
-                    styles.button,
-                ]}
+              onPress={onDeletePress}
+              testID="delete-course"
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed ? "#450006" : "transparent",
+                },
+                styles.button,
+              ]}
             >
-                <FontAwesome5 name="trash-alt" size={30} color="#FFFFFF" />
+              <FontAwesome5 name="trash-alt" size={30} color="#FFFFFF" />
             </Pressable>
-        </>
-        }
+          </>
+        )}
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Class
+export default Class;
 
 const styles = StyleSheet.create({
-    course: {
-        width: "100%",
-        backgroundColor: "#73000A",
-        borderWidth: 3,
-        borderColor: "#000000",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 20,
-        gap: 10,
-        padding: 5
-    },
+  course: {
+    width: "100%",
+    backgroundColor: "#73000A",
+    borderWidth: 3,
+    borderColor: "#000000",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    gap: 10,
+    padding: 5,
+  },
 
-    courseText: {
-        width: "90%",
-        flex: 1,
-        flexShrink: 0
-    },
+  courseText: {
+    width: "90%",
+    flex: 1,
+    flexShrink: 0,
+  },
 
-    courseHeader: {
-        color: "#FFFFFF",
-        fontSize: 30
-    },
+  courseHeader: {
+    color: "#FFFFFF",
+    fontSize: 30,
+  },
 
-    courseInfo: {
-        color: "#FFFFFF",
-        fontSize: 20,
-        flexShrink: 0
-      },
+  courseInfo: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    flexShrink: 0,
+  },
 
-    courseIcons: {
-        flexDirection: "row",
-        gap: 10,
-        alignItems: "center"
-    },
+  courseIcons: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
 
-    button: {
-        padding: 5,
-    },
-})
+  button: {
+    padding: 5,
+  },
+});
